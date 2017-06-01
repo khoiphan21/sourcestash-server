005254117cbc3adac8240db63469b13faadb5b22
Khoi Phan <khoiphan21@gmail.com>
2017-06-01 18:43:41 +1000

    added some debugging logs

--- services/mysql.js
+++ services/mysql.js
@@ -4,6 +4,7 @@ let mysqlModule = require('mysql');
 
 let config;
 if (process.env.INSTANCE_CONNECTION_NAME) {
+    console.log('Connecting to remote database');
     config = {
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
@@ -11,6 +12,7 @@ if (process.env.INSTANCE_CONNECTION_NAME) {
     }
     config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
 } else {
+    console.log('Connecting to local database');
     config = {
         host: 'localhost',
         user: 'root',
