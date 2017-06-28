d7c85148b7ec5859103ec3f3cd6b0e8d7e45763d
Jeremy Ng Yung Hau <nicholas5352@gmail.com>
2017-06-28 17:05:23 +1000

    card service

---
+++ services/mysql1.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let mysqlModule = require('mysql');
+
+let config;
+if (process.env.INSTANCE_CONNECTION_NAME) {
+    console.log('Connecting to remote database');
+    config = {
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE
+    }
+    config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+} else {
+    console.log('Connecting to local database');
+    config = {
+        host: 'localhost',
+        user: 'root',
+        database: 'cardboard'
+    }
+}
+
+var mysql = mysqlModule.createConnection(config);
+
+mysql.connect(function(err) {
+    if (err) {
+        console.log('Error connecting to Db');
+        throw err;
+    }
+    console.log('Database connection established');
+});
+
+module.exports = mysql;
+
+// mysql.end(function(err) {
+//     // The connection is terminated gracefully
+//     // Ensures all previously enqueued queries are still
+//     // before sending a COM_QUIT packet to the MySQL server.
+// });
---
+++ services/mysqlPromise1.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var logic = require('./logic');
+var mysql = require('./mysql1');
+
+function query(queryString, inserts) {
+    if (inserts) {
+        return new Promise((resolve, reject) => {
+            mysql.query(queryString, inserts, (error, results) => {
+                if (error) reject(error);
+                resolve(results);
+            });
+        });
+    } else {
+        return new Promise((resolve, reject) => {
+            mysql.query(queryString, (error, results) => {
+                if (error) reject(error);
+                resolve(results);
+            });
+        });
+    }
+
+}
+
+function format(query, inserts) {
+    return mysql.format(query, inserts);
+}
+
+module.exports = {
+    query: query,
+    format: format
+}
